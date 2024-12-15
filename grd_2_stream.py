3dfc2c520fdf012cf19570f7659b6bc20490d6a0
Maxim <maxim.poliakov@stud.tu-darmstadt.de>
2024-12-15 12:22:31 +0100

    adjust plugin builder template

--- grd_2_stream.py
+++ grd_2_stream.py
@@ -21,9 +21,11 @@
  *                                                                         *
  ***************************************************************************/
 """
+from PyQt5.QtWidgets import QToolBar
 from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction
+from qgis.gui import QgisInterface
 
 # Initialize Qt resources from file resources.py
 from .resources import *
@@ -35,7 +37,7 @@ import os.path
 class Grd2Stream:
     """QGIS Plugin Implementation."""
 
-    def __init__(self, iface):
+    def __init__(self, iface: QgisInterface):
         """Constructor.
 
         :param iface: An interface instance that will be passed to this class
@@ -44,44 +46,16 @@ class Grd2Stream:
         :type iface: QgsInterface
         """
         # Save reference to the QGIS interface
+        self.sfp_toolbar = None
         self.iface = iface
         # initialize plugin directory
         self.plugin_dir = os.path.dirname(__file__)
-        # initialize locale
-        locale = QSettings().value('locale/userLocale')[0:2]
-        locale_path = os.path.join(
-            self.plugin_dir,
-            'i18n',
-            'Grd2Stream_{}.qm'.format(locale))
-
-        if os.path.exists(locale_path):
-            self.translator = QTranslator()
-            self.translator.load(locale_path)
-            QCoreApplication.installTranslator(self.translator)
 
         # Declare instance attributes
         self.actions = []
-        self.menu = self.tr(u'&Grd2Stream ')
+        self.menu = u'&Grd2Stream '
 
-        # Check if plugin was started the first time in current QGIS session
-        # Must be set in initGui() to survive plugin reloads
-        self.first_start = None
-
-    # noinspection PyMethodMayBeStatic
-    def tr(self, message):
-        """Get the translation for a string using Qt translation API.
-
-        We implement this ourselves since we do not inherit QObject.
-
-        :param message: String for translation.
-        :type message: str, QString
-
-        :returns: Translated version of message.
-        :rtype: QString
-        """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-        return QCoreApplication.translate('Grd2Stream', message)
-
+        self.pluginMenu = self.iface.pluginMenu().addMenu(QIcon(":/plugins/grd_2_stream/icon.png"), "&Grd2Stream")
 
     def add_action(
         self,
@@ -90,7 +64,7 @@ class Grd2Stream:
         callback,
         enabled_flag=True,
         add_to_menu=True,
-        add_to_toolbar=True,
+        add_to_toolbar_sfp_toolbar=False,
         status_tip=None,
         whats_this=None,
         parent=None):
@@ -144,14 +118,14 @@ class Grd2Stream:
         if whats_this is not None:
             action.setWhatsThis(whats_this)
 
-        if add_to_toolbar:
+        if add_to_toolbar_sfp_toolbar:
+            self.sfp_toolbar.addAction(action)
             # Adds plugin icon to Plugins toolbar
+        else:
             self.iface.addToolBarIcon(action)
 
         if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
+            self.pluginMenu.addAction(action)
 
         self.actions.append(action)
 
@@ -159,42 +133,33 @@ class Grd2Stream:
 
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
+        add_to_toolbar_sfp_toolbar = False
+        self.sfp_toolbar = self.iface.mainWindow().findChild(QToolBar, "ScienceFlightPlanner")
+        #for c in self.iface.mainWindow().findChildren(QToolBar, "ScienceFlightPlanner"):
+         #   if isinstance(c, QToolBar):
+          #      print(str(c) + c.objectName())
+
+        print(self.sfp_toolbar)
+        if not self.sfp_toolbar is None:
+            add_to_toolbar_sfp_toolbar = True
 
         icon_path = ':/plugins/grd_2_stream/icon.png'
         self.add_action(
             icon_path,
-            text=self.tr(u'grd2stream Menu Item'),
+            text=u'grd2stream Menu Item',
             callback=self.run,
-            parent=self.iface.mainWindow())
-
-        # will be set False in run()
-        self.first_start = True
+            parent=self.iface.mainWindow(),
+            add_to_toolbar_sfp_toolbar=add_to_toolbar_sfp_toolbar
+        )
 
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
         for action in self.actions:
-            self.iface.removePluginMenu(
-                self.tr(u'&Grd2Stream '),
-                action)
             self.iface.removeToolBarIcon(action)
+        self.iface.pluginMenu().removeAction(self.pluginMenu.menuAction())
 
 
     def run(self):
         """Run method that performs all the real work"""
-
-        # Create the dialog with elements (after translation) and keep reference
-        # Only create GUI ONCE in callback, so that it will only load when the plugin is started
-        if self.first_start == True:
-            self.first_start = False
-            self.dlg = Grd2StreamDialog()
-
-        # show the dialog
-        self.dlg.show()
-        # Run the dialog event loop
-        result = self.dlg.exec_()
-        # See if OK was pressed
-        if result:
-            # Do something useful here - delete the line containing pass and
-            # substitute with your code.
-            pass
+        print("Calculate...")
